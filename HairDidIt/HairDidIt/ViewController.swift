d367021c3ce86d7cbab5593e1e30c782510ea1f5
bcchandler <bcchandler@gmail.com>
2016-06-25 18:53:04 -0400

    Initial setup of project
    Just did file new so JJARDIM can grab it

---
+++ HairDidIt/HairDidIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HairDidIt
+//
+//  Created by Chris Chandler on 6/25/16.
+//  Copyright © 2016 Chris Chandler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
